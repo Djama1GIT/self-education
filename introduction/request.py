cd226b297dd7228d372fd36c59d5127198e85b8c
Dj.ama1 <dj.ama1@mail.ru>
2022-02-16 20:39:23 +0300

    introduction deleted

--- introduction/request.py
+++
@@ -1,3 +0,0 @@
-import requests
-r = requests.get("https://stepic.org/media/attachments/course67/3.6.2/273.txt")
-print(len(r.text.splitlines()))
--- introduction/request2.py
+++
@@ -1,5 +0,0 @@
-import requests
-r = requests.get("https://stepic.org/media/attachments/course67/3.6.3/699991.txt")
-while r.text[:2] != "We":
-    r = requests.get("https://stepic.org/media/attachments/course67/3.6.3/"+r.text)
-print(r.text)
