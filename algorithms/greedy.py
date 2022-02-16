f145c8cef17d8c952eb4a4eed5cedfe7ffafeca0
Dj.ama1 <dj.ama1@mail.ru>
2022-02-16 20:54:09 +0300

    greedy.py v0.2

--- algorithms/greedy.py
+++ algorithms/greedy.py
@@ -2,14 +2,10 @@ sets = []
 for i in range(int(input())):
     a, b = map(int, input().split())
     sets.append([a, b])
-sorted_sets = sorted(sets, key=lambda i: i[1])
-points = [sorted_sets.pop(0)[1]]
-while len(sorted_sets) != 0:
-    while points[-1] in range(sorted_sets[0][0], sorted_sets[0][1] + 1):
-        del(sorted_sets[0])
-        if len(sorted_sets) == 0:
-            break
-    if sorted_sets:
-        points += [sorted_sets.pop(0)[1]]
+sorted_sets = sorted(sets, reverse=True)
+points = []
+for ps in sorted_sets:
+    if not points or ps[1] < points[-1]:
+        points.append(ps[0])
 print(len(points))
 print(*points)
