e574fcecf5425a07b80ceb79c62bfd9374ba8d53
Dj.ama1 <dj.ama1@mail.ru>
2022-03-10 11:36:29 +0300

    class.py v2

--- basics/class.py
+++ basics/class.py
@@ -13,11 +13,30 @@ class MoneyBox:
             return False
 
 
+class Buffer:
+    def __init__(self):
+        self.arr = []
+
+    def add(self, *a):
+        self.arr += list(a)
+        while len(self.arr) >= 5:
+            print(sum(self.arr[:5]))
+            self.arr = self.arr[5:]
+
+    def get_current_part(self):
+        return self.arr
+
+
 def test():
-    x = MoneyBox(10)
-    x.add(5)
-    x.add(5)
-    print(x.can_add(1))
+    buf = Buffer()
+    buf.add(1, 2, 3)
+    buf.get_current_part()
+    buf.add(4, 5, 6)
+    buf.get_current_part()
+    buf.add(7, 8, 9, 10)
+    buf.get_current_part()
+    buf.add(1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1)
+    buf.get_current_part()
 
 
 if __name__ == "__main__":
