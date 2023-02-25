d119a350d411335ddc90e24ad7fcce0475b1c78a
Dj.ama1 <dj.ama1@mail.ru>
2023-02-25 12:41:09 +0300

    deque.py

--- DS/deque.py
+++ DS/deque.py
@@ -1,5 +1,5 @@
-# from collections import deque
-
+from collections import deque
+import sys
 # dq = deque([1, 2, 3, 4, 5], maxlen=5)  # deque([1, 2, 3, 4, 5], maxlen=5) O(n)
 # dq.append(6)  # deque([2, 3, 4, 5, 6], maxlen=5) O(1)
 # dq.appendleft(1)  # deque([1, 2, 3, 4, 5], maxlen=5) O(1)
@@ -19,6 +19,25 @@
 # dq.rotate(1)  # deque([4, 3, 2, 1, 1], maxlen=5) O(k)
 # f = dq.copy()  # deque([4, 3, 2, 1, 1], maxlen=5) O(n)
 # dq.clear()  # deque([], maxlen=5)
-lst_in = list(map(int, input().split()))  # 1 2 3 5 4 11 7 23 24 25 19 23
-q = __import__("collections").deque(lst_in)  # deque([1, 2, 3, 5, 4, 11, 7, 23, 24, 25, 19, 23])
-print(*[q.popleft() for i in range(3)])  # 1 2 3 # deque([5, 4, 11, 7, 23, 24, 25, 19, 23])
+# lst_in = list(map(int, input().split()))  # 1 2 3 5 4 11 7 23 24 25 19 23
+# q = deque(lst_in)  # deque([1, 2, 3, 5, 4, 11, 7, 23, 24, 25, 19, 23])
+# print(*[q.popleft() for i in range(3)])  # 1 2 3 # deque([5, 4, 11, 7, 23, 24, 25, 19, 23])
+
+# lst_in = list(map(str.strip, input().split()))  # этот список в программе не менять
+# q = deque(lst_in)
+# q.insert(2, "run")
+# q.remove("edit")
+# print(q)
+
+# data = list(map(int, input().split()))  # этот список в программе не менять
+# buff = deque(list(reversed(data))[:10], maxlen=10)
+# print(*[buff.pop() for i in range(3)])
+
+
+# lst_in = list(map(str.strip, sys.stdin.readlines()))
+# back_url = deque(lst_in, maxlen=20)
+# print(back_url.pop())
+
+# data = list(map(int, input().split()))
+# lifo = data[:]
+# print(*[lifo.pop() for i in range(2)])
